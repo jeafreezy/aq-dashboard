11faf736e8a88ab1f6e3e22991494e9114cd95c0
jeafreezy <jolaiyaemmanuel@gmail.com>
2024-10-26 21:55:28 +0200

    Revert "Revert "dashboard workshop""
    This reverts commit b4288c1bde02667916d3def4069ed02c369ae983.

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,7 @@ const WORLD_AIR_QUALITY_API_TOKEN: string =
 /**
  * This is a Typescript type for a single station.
  */
+
 type TStation = {
   uid: string;
   aqi: string;
@@ -101,6 +102,7 @@ async function populateMarkers(
         const source = map.getSource("stations") as maplibregl.GeoJSONSource;
         source.setData(geoJSON);
       } else {
+
         map.addSource("stations", {
           type: "geojson",
           data: geoJSON,
@@ -290,7 +292,7 @@ function App() {
         style:
           "https://tiles.basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json", // The url to the style i.e basemap that is displayed.
         center: [20.11, 49.35], // The center of the map around Europe.
-        zoom: 4, // The zoom level of the map.
+        zoom: 10, // The zoom level of the map.
       })
         .addControl(
           // To zoom in and out.
@@ -543,7 +545,7 @@ function App() {
   return (
     <main className="bg-black text-white">
       <header className="h-14 flex items-center px-6 ">
-        <p>Global Real-Time Air Quality Monitoring Dashboard</p>
+        <p>My Air Quality Monitoring Dashboard</p>
       </header>
       <div className="w-full h-screen grid grid-cols-9">
         {/* This is the sidebar where the information about the air quality is presented.*/}
